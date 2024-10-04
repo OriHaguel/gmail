210f827058cd91200aeba8647b5243e6ae5ba201
OriHaguel <orihaguel@gmail.com>
2024-10-04 14:25:30 +0300

    Add server

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
   build: {
     // outDir: '../backend/public',
     // emptyOutDir: true,
